900b0e860e92c490d52789b3375fbf9d32f13291
musekorea <musekorea@hanmail.net>
2022-02-12 20:06:41 +0800

    layout

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import styles from "./Main.module.css";
+
+const Main = () => {
+	return (
+		<div className={styles.main}>
+			<h3 className={styles.title}>20% Off</h3>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed totam
+				rerum delectus magnam aliquid harum doloribus corporis laborum optio,
+				expedita est illo commodi quaerat ipsam inventore animi quibusdam
+				repellat veniam.
+			</p>
+			<button type="button" className="btn btn-primary">
+				Learn More
+			</button>
+		</div>
+	);
+};
+
+export default Main;
